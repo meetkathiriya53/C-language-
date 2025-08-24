26479ab2629e3b3704866002181286744b9e8468
meetkathiriya53 <meetkathiriya45@gmail.com>
2025-08-25 01:13:46 +0530

    Add files via upload

---
+++ First 10 even num.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+int a=1,b=0;
+for(;a<=10;a++){
+printf("%d\n",b);
+b+=2;
+}
+}
---
+++ First 10 odd num.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+int a=0,b=1;
+for(;a<=10;a++){
+printf("%d\n",b);
+b+=2;
+}
+}
