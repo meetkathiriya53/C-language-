2f4f1b0100334f9719916fc5dc0ca7fdd19c9d50
meetkathiriya53 <meetkathiriya45@gmail.com>
2025-09-10 17:41:03 +0530

    Add files via upload

---
+++ bubble inc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+    int a[5], n;
+    for(int i = 0; i < 5; i++) {
+        printf("enter the number :");
+        scanf("%d", &a[i]);
+    }
+    for(int j = 0; j < 5; j++) {
+        for(int i = 0; i < 4 - j; i++) {
+            if(a[i] > a[i+1]) {   
+                n = a[i];
+                a[i] = a[i+1];
+                a[i+1] = n;
+            }
+        }
+    }
+    for(int i = 0; i < 5; i++)
+        printf("%d ", a[i]);
+}
